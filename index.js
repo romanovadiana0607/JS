d75164cfbdd64218191a0990cacc78a1e9f4ddd8
romanovadiana0607 <romanovadi0607@gmail.com>
2023-04-06 17:40:48 +0300

    commit

---
+++ index.js
@@ -0,0 +1,46 @@
+// 1
+let name;
+let admin;
+name = "John";
+admin = "John";
+alert(admin);
+
+
+// 2
+let earth;
+let username;
+console.info(earth, username)
+
+// 3
+let age = prompt("How old are you?");
+let gender = prompt("What is your gender?");
+let height = prompt("How tall are you?");
+age = 25;
+gender = "female";
+height = "165cm";
+console.warn(age);
+console.warn(gender);
+console.warn(height);
+
+
+// 4
+let yourname = prompt("What is your name?")
+let yourage = prompt("How old are you?")
+if (yourage >= 18) {
+    console.info("Иди гулять")
+}
+else {
+    console.info("Оставайся дома")
+}
+
+
+// 5
+let number = prompt("Insert your nuber")
+if (number >= 10 && number <= 1100){
+    console.info("Входит")
+
+}
+else{
+    console.info("Не Входит")
+}
+
