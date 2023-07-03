a6b9f77ae3d218fd4a06dbd422a0f772bc747fc8
romanovadiana0607 <romanovadi0607@gmail.com>
2023-07-04 01:45:50 +0300

    c

--- index.js
+++ index.js
@@ -1,46 +1,71 @@
+// setTimeout
+//1
+function printDelayedString(string) {
+    setTimeout(() => {
+      console.log(string);
+    }, 5000); 
+  }
+  printDelayedString("My name is Diana");
+
+  //2
+  function fetchDataFromServer() {
+    setTimeout(() => {
+      const newData = "New data";
+      console.log(newData);
+
+    }, 10000); 
+  }
+fetchDataFromServer();
+
+//setInterval:
+//1
+
+function getTimeLeftUntilEndOfDay() {
+    const now = new Date();
+    const endOfDay = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 23, 59, 59);
+    const timeLeft = endOfDay.getTime() - now.getTime();
+  
+    const hoursLeft = Math.floor(timeLeft / (1000 * 60 * 60));
+    const minutesLeft = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    const secondsLeft = Math.floor((timeLeft % (1000 * 60)) / 1000);
+  
+    console.log(`Time left: ${hoursLeft} hours, ${minutesLeft} min, ${secondsLeft} sec.`);
+  }
+  
+  setInterval(getTimeLeftUntilEndOfDay, 5000);
+  
+//Date
 // 1
-let name;
-let admin;
-name = "John";
-admin = "John";
-alert(admin);
+function getCurrentTime() {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    return `${hours}:${minutes}:${seconds}`;
+  }
+  
+  // 2
+  function getCurrentDate() {
+    const now = new Date();
+    const day = String(now.getDate()).padStart(2, '0');
+    const month = String(now.getMonth() + 1).padStart(2, '0'); 
+    const year = now.getFullYear();
+    return `${day}.${month}.${year}`;
+  }
+  
+  //3
+  function getDaysUntilNewYear() {
+    const now = new Date();
+    const currentYear = now.getFullYear();
+    const newYear = new Date(currentYear + 1, 0, 1); 
+    const timeLeft = newYear.getTime() - now.getTime();
+    const daysLeft = Math.ceil(timeLeft / (1000 * 60 * 60 * 24)); 
+    return daysLeft;
+  }
 
+  console.log("Time now:", getCurrentTime());
+  console.log("Date right now:", getCurrentDate());
+  console.log("Days until new year:", getDaysUntilNewYear());
+  
 
-// 2
-let earth;
-let username;
-console.info(earth, username)
-
-// 3
-let age = prompt("How old are you?");
-let gender = prompt("What is your gender?");
-let height = prompt("How tall are you?");
-age = 25;
-gender = "female";
-height = "165cm";
-console.warn(age);
-console.warn(gender);
-console.warn(height);
-
-
-// 4
-let yourname = prompt("What is your name?")
-let yourage = prompt("How old are you?")
-if (yourage >= 18) {
-    console.info("Иди гулять")
-}
-else {
-    console.info("Оставайся дома")
-}
-
-
-// 5
-let number = prompt("Insert your nuber")
-if (number >= 10 && number <= 1100){
-    console.info("Входит")
-
-}
-else{
-    console.info("Не Входит")
-}
-
+  
